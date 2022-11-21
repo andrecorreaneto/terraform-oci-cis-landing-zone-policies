ae294b60187d69414030c02a659018d3be34b3dc
Andre Correa <andre.correa@oracle.com>
2022-11-21 15:02:56 -0300

    refact: old example removed

--- examples/departments-environments/variables.tf
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2022 Oracle and/or its affiliates.
-# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-
-variable "tenancy_id" {}
-variable "user_id" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
-variable "private_key_password" {}
-variable "home_region" {}
